7ffb9992d23d330af073de7767254894c353f893
pingwindyktator <ja2222ja@gmail.com>
2018-08-24 11:42:38 +0200

    Fix compilation problems

--- cgo_flags.go
+++ cgo_flags.go
@@ -5,7 +5,7 @@
 package main
 
 /*
-#cgo !windows LDFLAGS: -L/opt/cuda/lib64 -L/opt/cuda/lib -lcuda -lcudart -lstdc++ obj/decred.a
+#cgo !windows LDFLAGS: -L/opt/cuda/lib64 -L/opt/cuda/lib -L/usr/local/cuda/lib64 obj/decred.a -lcuda -lcudart -lstdc++ -ldl
 #cgo windows LDFLAGS: -Lobj -ldecred -Lnvidia/CUDA/v7.0/lib/x64 -lcuda -lcudart -Lnvidia/NVSMI -lnvml
 */
 import "C"
--- nvml/cgo_flags.go
+++ nvml/cgo_flags.go
@@ -1,7 +1,7 @@
 package nvml
 
 /*
-#cgo !windows LDFLAGS: -lnvidia-ml
+#cgo !windows LDFLAGS: -L/opt/cuda/lib64 -L/opt/cuda/lib -L/usr/local/cuda/lib64 -L/usr/lib/nvidia-396 -lnvidia-ml
 #cgo windows LDFLAGS: -L../nvidia/NVSMI/ -lnvml
 */
 import "C"
