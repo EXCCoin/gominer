710c6b9ad3142352c3e7f1736cf4e6cdc30ed214
pingwindyktator <ja2222ja@gmail.com>
2018-10-03 13:04:31 +0200

    Update build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" .\eqcuda1445\eqcuda1445.sln /Build "Release|x64"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" .\eqcuda1445\eqcuda1445.sln /Rebuild "Release|x64"
 
 copy .\eqcuda1445\x64\Release\eqcuda1445.dll .\eqcuda1445.dll
 copy .\eqcuda1445\x64\Release\eqcuda1445.lib .\eqcuda1445.lib
